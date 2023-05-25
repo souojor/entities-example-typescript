e219c1287bc42ab5b32cfaafb610852be50b5593
souojor <souojor@gmail.com>
2023-05-25 13:32:28 -0300

    product and tests

--- src/entity/order.spec.ts
+++ src/entity/order.spec.ts
@@ -29,12 +29,21 @@ describe("Order unit tests", () => {
 
     it("should calculate total", () => {
 
-        const item = new OrderItem("i1", "Item 1", 100);
-        const item2 = new OrderItem("i2", "Item 2", 200);
+        const item = new OrderItem("i1", "Item 1", 100, "p1", 1);
+        const item2 = new OrderItem("i2", "Item 2", 200, "p2", 2);
         const order = new Order("o1", "c1", [item, item2]);
 
         const total = order.total();
 
-        expect(total).toBe(300);
+        expect(total).toBe(500);
+    });
+
+    it("should throw error if the item quantity is less or equal 0", () => {
+
+        expect(() => {
+            const item = new OrderItem("i1", "Item 1", 100, "p1", 0);
+            const order = new Order("o1", "c1", [item]);
+        }).toThrowError("Quantity must be greater than 0.");
+
     });
 });
